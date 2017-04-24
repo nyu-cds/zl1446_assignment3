3c9835e1bb9a042fcd391ab4c4a33f9043c866a9
Zewei Liu <zl1446@nyu.edu>
2017-04-23 22:26:02 -0400

    assignment11

--- assignment11/test.py
+++ assignment11/test.py
@@ -17,7 +17,7 @@ class Test_generate_data2process(unittest.TestCase):
 	def test_mpi_sort_data(self): 
 	# This test case comes from Assignment 11
 		dataset = np.array([3,5,7,4,6,7,11,9,2,8,3,2])
-		self.assertTrue(generate_data2process(dataset).tolist(), np.array([[2,2], [3,5,4,3], [7,6,7,8], [9, 11]]).tolist() )
+		self.assertTrue(generate_data2process(dataset), np.array([[2,2], [3,5,4,3], [7,6,7,8], [9, 11]]) )
 
 if __name__ == '__main__':
 	unittest.main()
